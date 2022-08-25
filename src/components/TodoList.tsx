b2b761f1ab41a918179aeea95b37fd195b6137db
Radek Kaluža <kaluza.radek@gmail.com>
2022-08-25 15:05:31 +0200

    feat: initial commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,50 @@
+import './TodoList.css'
+
+import React from 'react'
+import { Todo } from '../model'
+import { SingleTodo } from './SingleTodo'
+import { Droppable } from 'react-beautiful-dnd'
+
+interface Props {
+  todos: Todo[]
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+  completedTodos: Todo[]
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+export const TodoList: React.FC<Props> = ({ todos, setTodos, completedTodos, setCompletedTodos }) => {
+  return (
+    <div className="container">
+      <Droppable droppableId="TodosList">
+        {(provided, snapshot) => (
+          <div
+            className={`todos ${snapshot.isDraggingOver ? 'dragactive' : ''}`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos__heading">Active tasks</span>
+            {todos?.map((todo, index) => (
+              <SingleTodo index={index} todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId="TodosRemove">
+        {(provided, snapshot) => (
+          <div
+            className={`todos ${snapshot.isDraggingOver ? 'dragcomplete' : 'remove'}`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos__heading">Completed tasks</span>
+            {completedTodos?.map((todo, index) => (
+              <SingleTodo index={index} todo={todo} key={todo.id} todos={completedTodos} setTodos={setCompletedTodos} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  )
+}
